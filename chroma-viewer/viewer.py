dcf606ec4aec82d8456cc2fa8a2443776f1d99e4
Chris Agostino <cjp.agostino@gmail.com>
2025-01-24 09:08:46 -0500

    adjusted viewer to be up to date with current chroma client api for the list_collections and get_collection. also adjusted the viewer so that it will show a truncated form of the embeddings rather than showing none to enable easier verification

--- chroma-viewer/viewer.py
+++ chroma-viewer/viewer.py
@@ -1,14 +1,17 @@
 import chromadb
 from chromadb.config import Settings
 from chromadb.utils import embedding_functions
-import pandas as pd 
+import pandas as pd
 import streamlit as st
 import argparse
 
-parser = argparse.ArgumentParser(description='Set the Chroma DB path to view collections')
-parser.add_argument('db')
+parser = argparse.ArgumentParser(
+    description="Set the Chroma DB path to view collections"
+)
+parser.add_argument("db")
 
-pd.set_option('display.max_columns', 4)
+pd.set_option("display.max_columns", 4)
+
 
 def view_collections(dir):
     st.markdown("### DB Path: %s" % dir)
@@ -21,17 +24,23 @@ def view_collections(dir):
 
     st.header("Collections")
 
-    for collection in client.list_collections():
-        data = collection.get()
+    for coll in client.list_collections():
+        print("getting data for collection: %s" % coll)
+        collection = client.get_collection(
+            coll,
+        )
 
-        ids = data['ids']
+        data = collection.get(include=["embeddings"])
+        ids = data["ids"]
         embeddings = data["embeddings"]
+        data = collection.get()
         metadata = data["metadatas"]
         documents = data["documents"]
-
         df = pd.DataFrame.from_dict(data)
+        df["embeddings"] = str(embeddings)
         st.markdown("### Collection: **%s**" % collection.name)
         st.dataframe(df)
+
 
 if __name__ == "__main__":
     try:
@@ -40,4 +49,3 @@ if __name__ == "__main__":
         view_collections(args.db)
     except:
         pass
-
